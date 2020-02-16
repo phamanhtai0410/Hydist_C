eb5bd55559e234e0dfc91d360cbbd7f761dfb7aa
Tran Minh Quan <quantm@unist.ac.kr>
2020-02-16 18:50:02 +0900

    Volta

--- support_funcs.cpp
+++ support_funcs.cpp
@@ -252,7 +252,7 @@ __global__ void Find_Calculation_limits_Horizontal( Argument_Pointers *arg, Cons
 	int start = 2;
 	int end = 0;
 	int offset = M + 3;
-	if (i == (N-1)
+	if (i == (N-1))
 		Interpolate_FS_ng(N - 1, N - 1, -1, arg, coeffs);
 	if (i == 2)
 		Interpolate_FS_ng(2, 2, 1, arg, coeffs);
